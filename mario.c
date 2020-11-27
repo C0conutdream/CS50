789b3ca90d009431fa609ce9faeb7b501574d8d9
C0conutdream <C0conutdream@users.noreply.github.com>
2020-11-27 04:19:34 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,18 +6,18 @@ int main(void)
     int height;
     do
     {
-    height = get_int ("height of pyrimid:\n" );
+    height = get_int("height of pyrimid:\n");
     }
-   while (height < 1 || height > 8 );
+   while (height < 1 || height > 8);
    if (height > 0 || height < 8)
   {
-    for (int row = 0; row < height; row++ )
+    for (int row = 0; row < height; row++)
    {
-     for (int space = (height - row) ; space > 1; space -- )
+     for (int space = (height - row) ; space > 1; space --)
      {
      printf (" ");
      }
-     for (int hash = 0; hash <= row; hash++ )
+     for (int hash = 0; hash <= row; hash++)
      {
      printf ("#");
      }
